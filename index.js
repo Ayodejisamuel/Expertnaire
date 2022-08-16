069414e7b79001558580d15e6eb8e5f16f688269
Faleye Ayodeji <ayodejisamuel80@gmail.com>
2022-08-16 08:54:21 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,6 @@
+var experience = prompt("What is Your Experience so far with Programming?");
+note = experience.length;
+alert("You have written " + note + " characters, you have " + (140-note) + " characters left");
+ 
+// alert(prompt("What is Your Experience so far with Programming?").slice(0,30));
+
