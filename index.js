e81f832c6413a4fc1bdb61ebc99586373afcdb8a
Faleye Ayodeji <ayodejisamuel80@gmail.com>
2022-08-22 18:31:11 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // var experience = prompt("What is Your Experience so far with Programming?");
 // note = experience.length;
-alert("This Website is created by Ayodeji. You gonna love it.");
+// alert("This Website is created by Ayodeji. You gonna love it.");
  
 // alert(prompt("What is Your Experience so far with Programming?").slice(0,30));
 
