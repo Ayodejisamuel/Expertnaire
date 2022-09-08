0585eccfe1c52d9bc048b1f37a64928b5f55586d
Faleye Ayodeji <ayodejisamuel80@gmail.com>
2022-09-08 16:50:58 -0700

    Update index.js

--- index.js
+++ index.js
@@ -4,3 +4,8 @@
  
 // alert(prompt("What is Your Experience so far with Programming?").slice(0,30));
 
+function upperCase() {
+   const x = document.getElementById("reno").innerHTML
+   x.value = x.value.toUpperCase();
+ 
+}
